363fe5bcf058544c4155a2dcde9c264582d01d7a
jladams <adamsjlp@gmail.com>
2016-11-15 15:01:45 -0500

    test and refine Rmd page with placeholder data

--- R/gather.R
+++ R/gather.R
@@ -2,14 +2,21 @@
 library(streamR)
 library(ROAuth)
 library(tidyverse)
+library(lubridate)
 
 load("./data/myoauth.Rdata")
 
 tweetTemp <- tempfile(fileext = ".json")
 
-filterStream(tweetTemp, track = "TuesdayMotivation", timeout = 30, oauth = myoauth, verbose = TRUE)
+filterStream(tweetTemp, track = "AmericaRecyclesDay", timeout = 300, oauth = myoauth, verbose = TRUE)
 
 df <- parseTweets(tweetTemp, simplify = TRUE) %>%
-  select(id_str, text, lang, geo_enabled, retweet_count, created_at, time_zone)
+  mutate(created = as.POSIXct(created_at, 
+                              format="%a %b %d %H:%M:%S +0000 %Y", 
+                              tz="GMT", 
+                              origin="1970-01-01"
+                              )
+         ) %>%
+  select(id_str, created, text, lang, geo_enabled, retweet_count, time_zone)
 
 write_csv(df, "./data/tweets.csv")
--- R/plot.R
+++ R/plot.R
@@ -1,7 +1,7 @@
 
 library(tidytext)
 library(tidyverse)
-
+library(lubridate)
 
 
 df <- read_csv("./data/tweets.csv", col_types = cols(id_str = col_character()))
@@ -12,4 +12,22 @@ df_tidy <- df %>%
 
 df_sentiment <- df_tidy %>%
   left_join(get_sentiments(lexicon = "nrc")) %>%
-  filter(!is.na(sentiment))
+  filter(!is.na(sentiment)) %>%
+  group_by(sentiment, day = wday(created, label = TRUE), time = floor_date(created,unit = "minutes")) %>%
+  summarize(frequency = n()) %>%
+  arrange(desc(frequency))
+
+sentiment_freq <- ggplot(df_sentiment, aes(x = sentiment, y = frequency)) +
+  geom_col(aes(fill = sentiment)) +
+  guides(fill = FALSE) +
+  facet_grid(.~day)
+
+print(sentiment_freq)
+
+sentiment_lines <- ggplot(df_sentiment, aes(x = time, y = frequency)) +
+  geom_line(aes(color = sentiment, group = sentiment)) +
+  geom_point(aes(color = sentiment)) +
+  facet_grid(day~.)
+
+print(sentiment_lines)
+
