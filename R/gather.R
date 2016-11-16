4e7b56910e4c05bd42b4a0269a26197b6db71dab
jladams <adamsjlp@gmail.com>
2016-11-16 12:02:28 -0500

    getting ready for automating proces

--- R/gather.R
+++ R/gather.R
@@ -4,12 +4,12 @@ library(ROAuth)
 library(tidyverse)
 library(lubridate)
 
-load("./data/myoauth.Rdata")
+load("/Users/jamesadams/projects/lita-stream/data/myoauth.Rdata")
 
 tweetTemp <- tempfile(fileext = ".json")
 
-filterStream(tweetTemp, track = "WednesdayWisdom", timeout = 30, oauth = myoauth, verbose = TRUE)
+filterStream(tweetTemp, track = "NationalFastFoodDay", timeout = 60, oauth = myoauth, verbose = TRUE)
 
 df_raw <- parseTweets(tweetTemp, simplify = TRUE)
 
-write_csv(df_raw, paste0("~/Box Sync/lita-tweets/", as.numeric(now()),"-raw.csv"))
+write_csv(df_raw, paste0("/Users/jamesadams/Box Sync/lita-tweets/", as.numeric(now()),"-raw.csv"))
