0c301f1cd282bf31f2c8e0a4f27c2d44377be932
jladams <adamsjlp@gmail.com>
2016-11-16 10:45:08 -0500

    add stats and overview

--- R/gather.R
+++ R/gather.R
@@ -8,7 +8,7 @@ load("./data/myoauth.Rdata")
 
 tweetTemp <- tempfile(fileext = ".json")
 
-filterStream(tweetTemp, track = "WednesdayWisdom", timeout = 60, oauth = myoauth, verbose = TRUE)
+filterStream(tweetTemp, track = "WednesdayWisdom", timeout = 30, oauth = myoauth, verbose = TRUE)
 
 df_raw <- parseTweets(tweetTemp, simplify = TRUE)
 
